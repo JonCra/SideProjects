452d747a657e0712a17a40d52859fd66ff491e53
Jonas Cram <jonas.d.cram@gmail.com>
2025-12-18 22:26:43 -0700

    maybe fixed problem?

--- DnD_DiceRollerV1/DnD_DiceRollerV1/obj/Debug/net8.0-windows/DnD_DiceRollerV1.AssemblyInfo.cs
+++ DnD_DiceRollerV1/DnD_DiceRollerV1/obj/Debug/net8.0-windows/DnD_DiceRollerV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DnD_DiceRollerV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc169e56237c7858fe2117b8a358da1cbc6e08fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98dcb29820600c836371ab3e24760c4c6f2638d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DnD_DiceRollerV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DnD_DiceRollerV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
